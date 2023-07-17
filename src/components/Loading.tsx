7a974e6ae202fd09a3c31d3183f8c05abc3485b5
Michael Byers <108441006+mike242424@users.noreply.github.com>
2023-07-17 15:21:13 -0400

    centered loading bar on page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,16 @@
+import { Box } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
 
 const Loading = () => {
   return (
-    <>
-      <CircularProgress size={60} color="secondary"/>
-    </>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <CircularProgress size={60} color="secondary" />
+    </Box>
   );
 };
 
