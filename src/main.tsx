b15d8cc03342253e85b41d25a04d867608456c6a
Michael Byers <108441006+mike242424@users.noreply.github.com>
2023-07-17 18:22:16 -0400

    removed index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
